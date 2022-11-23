7839b53c8a7f4c74f63de255d53509e0e301ec3c
Gegreenpeaced <107betriebshof@gmail.com>
2022-11-23 16:30:55 +0100

    Projektdateien hinzufügen.

---
+++ Mk-Docs-Server/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Mk_Docs_Server
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnInstallServer = new System.Windows.Forms.Button();
+            this.gbWorkspace = new System.Windows.Forms.GroupBox();
+            this.btnInstallAll = new System.Windows.Forms.Button();
+            this.btnSaveWorkspaceFiles = new System.Windows.Forms.Button();
+            this.btnInstallWorkspaceFiles = new System.Windows.Forms.Button();
+            this.btnInstallVSC = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.gbWorkspace.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(12, 185);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 0;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(450, 185);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnInstallServer
+            // 
+            this.btnInstallServer.Location = new System.Drawing.Point(6, 48);
+            this.btnInstallServer.Name = "btnInstallServer";
+            this.btnInstallServer.Size = new System.Drawing.Size(188, 23);
+            this.btnInstallServer.TabIndex = 2;
+            this.btnInstallServer.Text = "Server installieren";
+            this.btnInstallServer.UseVisualStyleBackColor = true;
+            // 
+            // gbWorkspace
+            // 
+            this.gbWorkspace.Controls.Add(this.btnInstallAll);
+            this.gbWorkspace.Controls.Add(this.btnSaveWorkspaceFiles);
+            this.gbWorkspace.Controls.Add(this.btnInstallWorkspaceFiles);
+            this.gbWorkspace.Controls.Add(this.btnInstallVSC);
+            this.gbWorkspace.Controls.Add(this.btnInstallServer);
+            this.gbWorkspace.Location = new System.Drawing.Point(325, 12);
+            this.gbWorkspace.Name = "gbWorkspace";
+            this.gbWorkspace.Size = new System.Drawing.Size(200, 167);
+            this.gbWorkspace.TabIndex = 3;
+            this.gbWorkspace.TabStop = false;
+            this.gbWorkspace.Text = "Arbeitsumgebung";
+            // 
+            // btnInstallAll
+            // 
+            this.btnInstallAll.Location = new System.Drawing.Point(6, 19);
+            this.btnInstallAll.Name = "btnInstallAll";
+            this.btnInstallAll.Size = new System.Drawing.Size(188, 23);
+            this.btnInstallAll.TabIndex = 6;
+            this.btnInstallAll.Text = "Alles installieren";
+            this.btnInstallAll.UseVisualStyleBackColor = true;
+            this.btnInstallAll.Click += new System.EventHandler(this.btnInstallAll_Click);
+            // 
+            // btnSaveWorkspaceFiles
+            // 
+            this.btnSaveWorkspaceFiles.Location = new System.Drawing.Point(6, 135);
+            this.btnSaveWorkspaceFiles.Name = "btnSaveWorkspaceFiles";
+            this.btnSaveWorkspaceFiles.Size = new System.Drawing.Size(188, 23);
+            this.btnSaveWorkspaceFiles.TabIndex = 5;
+            this.btnSaveWorkspaceFiles.Text = "Arbeitsdateien sichern";
+            this.btnSaveWorkspaceFiles.UseVisualStyleBackColor = true;
+            this.btnSaveWorkspaceFiles.Click += new System.EventHandler(this.btnSaveWorkspaceFiles_Click);
+            // 
+            // btnInstallWorkspaceFiles
+            // 
+            this.btnInstallWorkspaceFiles.Location = new System.Drawing.Point(6, 106);
+            this.btnInstallWorkspaceFiles.Name = "btnInstallWorkspaceFiles";
+            this.btnInstallWorkspaceFiles.Size = new System.Drawing.Size(188, 23);
+            this.btnInstallWorkspaceFiles.TabIndex = 4;
+            this.btnInstallWorkspaceFiles.Text = "Arbeitsdateien auswählen";
+            this.btnInstallWorkspaceFiles.UseVisualStyleBackColor = true;
+            this.btnInstallWorkspaceFiles.Click += new System.EventHandler(this.btnInstallWorkspaceFiles_Click);
+            // 
+            // btnInstallVSC
+            // 
+            this.btnInstallVSC.Location = new System.Drawing.Point(6, 77);
+            this.btnInstallVSC.Name = "btnInstallVSC";
+            this.btnInstallVSC.Size = new System.Drawing.Size(188, 23);
+            this.btnInstallVSC.TabIndex = 3;
+            this.btnInstallVSC.Text = "VSC installieren";
+            this.btnInstallVSC.UseVisualStyleBackColor = true;
+            this.btnInstallVSC.Click += new System.EventHandler(this.btnInstallVSC_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 217);
+            this.Controls.Add(this.gbWorkspace);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSettings);
+            this.Name = "frmMain";
+            this.Text = "MK Docs Server Manager";
+            this.gbWorkspace.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnInstallServer;
+        private System.Windows.Forms.GroupBox gbWorkspace;
+        private System.Windows.Forms.Button btnInstallAll;
+        private System.Windows.Forms.Button btnSaveWorkspaceFiles;
+        private System.Windows.Forms.Button btnInstallWorkspaceFiles;
+        private System.Windows.Forms.Button btnInstallVSC;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Mk-Docs-Server/frmMain.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO.Compression;
+using System.IO;
+
+namespace Mk_Docs_Server
+{
+    public partial class frmMain : Form
+    {
+        // ---------------
+        // Global Variables
+        // ---------------
+        public string atomDownloadPath = "https://nxcloud.norku.de/index.php/s/4QWfpGcrzs8mjEC";
+        public string mkdocsServerDownloadPath = "&nbsp;"
+        public string WorkspacePath;
+
+
+        // ----------------
+        // Main Form
+        // ----------------
+
+        public frmMain()
+        {
+            if (!System.IO.Directory.Exists("/Files")) // Datei ordner erstellen wenn nicht vorhanden
+            {
+                System.IO.Directory.CreateDirectory("/Files");
+            }
+                if (!System.IO.Directory.Exists("/Files/mkdocs")) // mk docs ordner erstellen wenn nicht vorhanden
+            {
+                System.IO.Directory.CreateDirectory("/Files/mkdocs");
+            }
+            InitializeComponent();
+        }
+
+        
+        // ----------------
+        // Form Buttons
+        // ----------------
+
+        private void btnInstallVSC_Click(object sender, EventArgs e)
+        {
+            InstallVSC();
+        }
+
+        private void btnInstallAll_Click(object sender, EventArgs e)
+        {
+            InstallMKDocsServer();
+            InstallVSC();
+            OpenWorkspacePath();
+
+        }
+
+        private void btnInstallWorkspaceFiles_Click(object sender, EventArgs e)
+        {
+            OpenWorkspacePath();
+        }
+
+        private void btnSaveWorkspaceFiles_Click(object sender, EventArgs e)
+        {
+            ZipWorkspaceFiles();
+        }
+
+
+        // ----------------
+        // Methods
+        // ----------------
+
+
+        public bool InstallMKDocsServer()
+        {
+            Application.Run(/*script.bat -mkdocs*/); // mkdocs mit pip3 installieren
+            return true;
+        }
+
+        public bool InstallVSC()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(atomDownloadPath, "/Files/atom-portable.zip");  // VSC Downloaden
+
+                using (var unzip = new Internals.Unzip("/Files/atom-portable.zip"))  // VSC entpacken
+                {
+                    unzip.ExtractToDirectory("/Files");
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool OpenWorkspacePath()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog // File Dialog spezifizieren
+                {
+                    InitialDirectory = @"D:\",
+                    Title = "Gezippte Arbeitsdateien",
+
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+
+                    DefaultExt = "zip",
+                    Filter = "zip files (*.zip)|*.zip",
+                    FilterIndex = 2,
+                    RestoreDirectory = true,
+
+                    ReadOnlyChecked = true,
+                    ShowReadOnly = true
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK) // Arbeitspfad angeben
+                {
+                    WorkspacePath = openFileDialog1.FileName;
+
+                    using (var unzip = new Internals.Unzip(WorkspacePath))
+                    {
+                        unzip.ExtractToDirectory("/Files/mkdocs"); // Datein entpacken und kopieren
+                    }
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool ZipWorkspaceFiles()
+        {
+            string ZipFileName = "/Files/" + DateTime.Now.ToString("ddMMyyyy-HHmmssfffff") + ".zip"; // Name des Zip Files
+            string PathToZip; // Pfad zum Zippen
+
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Auswahl des Speicherpfades
+            //to check user pressed OK button or CANCEL button in show dialog  
+            if (result == DialogResult.OK)
+            {
+                //Paste the selected folder path to textbox  
+                PathToZip = folderBrowserDialog1.SelectedPath;
+                ZipFile.CreateFromDirectory(PathToZip, ZipFileName); // zippen der Datei
+                return true;
+            }
+            return false;
+        }
+    }
+}
